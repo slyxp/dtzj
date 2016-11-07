5ee56b0d74e349bca60d30a694e5061ac889a9a6
liyang.sun <liyang.sun@tcl.com>
2016-11-07 17:15:00 +0800

    主页更多按钮点击HomePage跳转到主页

--- app/src/main/java/com/tcl/funday/WebViewActivity.java
+++ app/src/main/java/com/tcl/funday/WebViewActivity.java
@@ -1,7 +1,6 @@
 package com.tcl.funday;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.webkit.WebChromeClient;
