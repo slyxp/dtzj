bff0acd40d3ef386176bef78c81385406c30bafa
liyang.sun <liyang.sun@tcl.com>
2016-11-09 10:56:11 +0800

    设置导航栏颜色&参数

--- app/src/main/java/com/tcl/funday/ui/activity/MainActivity.java
+++ app/src/main/java/com/tcl/funday/ui/activity/MainActivity.java
@@ -1,13 +1,17 @@
 package com.tcl.funday.ui.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.tcl.funday.MyApplication;
 import com.tcl.funday.R;
 import com.tcl.funday.support.adapter.FragmentAdapter;
+import com.tcl.funday.utils.CommonUtils;
+
 import shanyao.tabpagerindictor.TabPageIndicator;
 
 public class MainActivity extends BaseActivity {
@@ -38,6 +42,12 @@ public class MainActivity extends BaseActivity {
         mViewPager.setAdapter(mFragmentAdapter);
         mIndicator.setViewPager(mViewPager);
         mIndicator.setIndicatorMode(TabPageIndicator.IndicatorMode.MODE_NOWEIGHT_NOEXPAND_NOSAME);
+        mIndicator.setDividerColor(getResources().getColor(R.color.themeColor));
+        mIndicator.setDividerPadding(CommonUtils.dip2px(MyApplication.getContext(), 10));
+        mIndicator.setIndicatorColor(getResources().getColor(R.color.themeColor));
+        mIndicator.setTextColor(getResources().getColor(R.color.navigation_tab_txt_color));
+        mIndicator.setTextColorSelected(getResources().getColor(R.color.themeColor));
+        mIndicator.setTextSize(CommonUtils.sp2px(MyApplication.getContext(), 16));
     }
 
     @Override
--- app/src/main/java/com/tcl/funday/utils/CommonUtils.java
+++ app/src/main/java/com/tcl/funday/utils/CommonUtils.java
@@ -30,6 +30,22 @@ public class CommonUtils {
     }
 
     /**
+     * px转化成sp
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sp转化成px
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (spValue * scale + 0.5f);
+    }
+
+    /**
      * 获取Resource对象
      */
     public static Resources getResources() {
